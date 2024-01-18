911df4637ae9942727e75b9bb7b67dd83e484ab9
Robert Castillo <roca1388@gmail.com>
2024-01-17 23:21:43 -0300

    update

--- src/pages/home/HomeBanner.tsx
+++ src/pages/home/HomeBanner.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Grid } from "@mui/material";
 
 const HomeBanner: React.FC = () => {
-  const banner="https://firebasestorage.googleapis.com/v0/b/pinguinos-kids.appspot.com/o/LogoMobile%2FHomeBanner%20(1).png?alt=media&token=dc9b9675-ee1e-4b0e-ad15-e2436352d979"
+  const banner="https://firebasestorage.googleapis.com/v0/b/pinguinos-kids.appspot.com/o/LogoMobile%2FHomeBanner%20.png?alt=media&token=90588c2d-f44a-49fc-8378-54bcdbccf712"
   return (
     <Grid container justifyContent="center" alignItems="center" marginTop="0px">
       <Grid item xs={12} lg={6}>
